23857aa28cd96961e2b40ed45e8068e9b0ec71f8
inittose <pol.konnik@gmail.com>
2024-04-05 20:30:38 +0700

    feature(lab1): Сверстал MainWindow

---
+++ Contacts/Contacts/Model/Contact.cs
@@ -0,0 +1,64 @@
+﻿namespace View.Model
+{
+    /// <summary>
+    /// Хранит информацию о контакте
+    /// </summary>
+    public class Contact
+    {
+        /// <summary>
+        /// Имя контакта.
+        /// </summary>
+        private string _name;
+
+        /// <summary>
+        /// Номер телефона.
+        /// </summary>
+        private string _phoneNumber;
+
+        /// <summary>
+        /// Электронная почта.
+        /// </summary>
+        private string _email;
+
+        /// <summary>
+        /// Возвращает и задает имя контакта.
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        /// <summary>
+        /// Возвращает и задает номер телефона.
+        /// </summary>
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set => _phoneNumber = value;
+        }
+
+        /// <summary>
+        /// Возвращает и задает электронную почту.
+        /// </summary>
+        public string Email
+        {
+            get => _email;
+            set => _email = value;
+        }
+
+        public Contact()
+        {
+            Name = string.Empty;
+            PhoneNumber = string.Empty;
+            Email = string.Empty;
+        }
+
+        public Contact(string name, string phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+    }
+}
