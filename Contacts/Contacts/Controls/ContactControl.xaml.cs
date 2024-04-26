c165f4cd6218626b04ef1796cdbbff8d86fe6e8c
inittose <pol.konnik@gmail.com>
2024-04-26 17:28:52 +0700

    feature(lab3): Внедрил ContactControl
    - изменил расположение ContactControl

---
+++ Contacts/Contacts/Controls/ContactControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace View.Controls
+{
+    /// <summary>
+    /// Interaction logic for ContactControl.xaml
+    /// </summary>
+    public partial class ContactControl : UserControl
+    {
+        public ContactControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
