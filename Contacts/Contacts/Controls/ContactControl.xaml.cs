a863a5f0bfa291529ce5bebb122ee788179b6dac
inittose <pol.konnik@gmail.com>
2024-05-04 18:21:10 +0700

    docs(View): Добавил комментарии

--- Contacts/Contacts/Controls/ContactControl.xaml.cs
+++ Contacts/Contacts/Controls/ContactControl.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using View.Model;
@@ -7,12 +6,12 @@ using View.Model;
 namespace View.Controls
 {
     /// <summary>
-    /// 
+    /// Хранит логику работы пользовательского интерфейса <see cref="ContactControl"/>.
     /// </summary>
     public partial class ContactControl : UserControl
     {
         /// <summary>
-        /// 
+        /// Создает экземпляр класса <see cref="ContactControl"/>.
         /// </summary>
         public ContactControl()
         {
@@ -20,10 +19,10 @@ namespace View.Controls
         }
 
         /// <summary>
-        /// 
+        /// Обрабатывает событие ввода символа в <see cref="TextBox"/>.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">Объект, вызвавший событие.</param>
+        /// <param name="e">Данные о событии.</param>
         private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             if (!Contact.PhoneNumberMask.Contains(e.Text))
@@ -33,10 +32,10 @@ namespace View.Controls
         }
 
         /// <summary>
-        /// 
+        /// Обрабатывает событие вставки символов в <see cref="TextBox"/>.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">Объект, вызвавший событие.</param>
+        /// <param name="e">Данные о событии.</param>
         private void TextBox_Pasting(object sender, DataObjectPastingEventArgs e)
         {
             var correctText = string.Empty;
--- Contacts/Contacts/MainWindow.xaml.cs
+++ Contacts/Contacts/MainWindow.xaml.cs
@@ -4,10 +4,13 @@ using View.ViewModel;
 namespace Contacts
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Хранит логику работы <see cref="MainWindow"/>.
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Создает экземпляр класса <see cref="MainWindow"/>.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
