d5bb459e3302d3f1063637b6198bac34d64754d0
inittose <pol.konnik@gmail.com>
2024-06-11 14:10:48 +0700

    feature(sln): Разделил проект View на три проекта

--- Contacts/ViewModel/ViewModel/MainVM.cs
+++ Contacts/ViewModel/ViewModel/MainVM.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Input;
 using System.Collections;
