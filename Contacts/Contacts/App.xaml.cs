f2e94dfa72c0a238cafadf96f6e0d50c39eb0576
Konstantin Polushvayko <pol.konnik@gmail.com>
2024-09-04 16:05:38 +0700

    fix(View): Внес правки
    - Удалил лишнее изображение photo_placeholder_100x100.png
    - Поменял пространство имен

--- Contacts/Contacts/App.xaml.cs
+++ Contacts/Contacts/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Data;
 using System.Windows;
 
 // TODO: удалить один файл photo_placeholder_100x100.png (дублируются в корне проекта и в папке Images)
+// UPD: Удалил, оставил в папке Images
 // TODO: Некорректный namespace
-namespace Contacts
+// Поправил
+namespace View
 {
     /// <summary>
     /// Interaction logic for App.xaml
